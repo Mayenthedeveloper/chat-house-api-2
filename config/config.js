1fc9dad2d2bc80026174d8d130e3bd86f916a54a
Mayen Akpan <neroxma@yahoo.com>
2021-03-03 17:36:27 -0800

    Adding Auth

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  APP_KEY:
+    process.env.APP_KEY ||
+    "be533ba5793eee628c666d53bc6e9a607e7dd2c78de3609d7915a8752260d04124de031d5c6844f702f6d1bc1bdcaf7fa67c14b47efdebd7300322ba6ef29f3e",
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,14 @@
 const User = require("../models").User;
 const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-// const config = require("../config/config");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    const secret = require("crypto").randomBytes(64).toString("hex");
     // find the user
     const user = await User.findOne({
       where: {
@@ -21,10 +23,38 @@ exports.login = async (req, res) => {
     if (!bcrypt.compareSync(password, user.password))
       return res.status(401).json({ message: "Incorrect password!" });
 
-    return res.send(user);
-  } catch (e) {}
+    // generate auth token
+    const userWithToken = generateToken(user.get({ raw: true }));
+
+    return res.send(userWithToken);
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
 
   return res.send([email, password]);
 };
 
-exports.register = async (req, res) => {};
+exports.register = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.create(req.body);
+
+    const userWithToken = generateToken(user.get({ raw: true }));
+    return res.send(userWithToken);
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+};
+
+const generateToken = (user) => {
+  console.log(user);
+  //   delete user.password;
+
+  const token = jwt.sign(user, config.APP_KEY, { expiresIn: 86400 });
+
+  return { ...{ user }, ...{ token } };
+};
